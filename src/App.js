9b56a51fd81eb8c7bdc6f709681d914776c7aea6
Ashraful Islam <ashrafunit7@gmail.com>
2021-03-03 23:22:00 +0600

    material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import "./App.css";
+import News from "./component/News/News";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=e26c8bb799a54820888e817b8fa53eee"
+    )
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Total Headlines: {articles.length}</h1>
+      {
+        articles.map(article =>  <News article={article}></News>)
+      }
     </div>
   );
 }
